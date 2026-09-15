aa0dfe5b64040417467630ee817ab0b3f94a5438
Yago Ferreira Corrêa <yagocmferreira9@gmail.com>
2026-09-14 23:30:23 -0300

    Create validate-gold.ps1

---
+++ scripts/validate-gold.ps1
@@ -0,0 +1,31 @@
+$ErrorActionPreference = "Stop"
+
+$projectRoot = Split-Path -Parent $PSScriptRoot
+
+Push-Location $projectRoot
+
+try {
+    docker compose up -d localstack
+
+    Write-Host "Validando Gold..."
+
+    docker compose `
+        --profile processing `
+        run `
+        --rm `
+        spark `
+        /opt/spark/bin/spark-submit `
+        --master "local[2]" `
+        --driver-memory "1g" `
+        --packages `
+        "io.delta:delta-spark_2.12:3.3.2,org.apache.hadoop:hadoop-aws:3.3.4" `
+        --conf "spark.jars.ivy=/tmp/.ivy2" `
+        /opt/demandflow/src/spark/gold/validate_gold.py
+
+    if ($LASTEXITCODE -ne 0) {
+        throw "Validação da Gold falhou."
+    }
+}
+finally {
+    Pop-Location
+}
